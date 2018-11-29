e08bfa0fcf851a35202802bd3bb77a9f165d3809
Alexander Sergeev <alexander.sergeev@firstlinesoftware.com>
2018-11-29 15:43:39 +0300

    testing x64

--- CPLEX/App.cs
+++ CPLEX/App.cs
@@ -10,27 +10,39 @@ public class App
     private static object locker = new object();
     public static void Main(string[] args)
     {
-        using (var writer = new StreamWriter("D:/VSProjects/CPLEX/stats.csv") { AutoFlush = true })
+        var fileName = "brock200_4.clq.txt";
+        Console.WriteLine(fileName);
+        var timer = Stopwatch.StartNew();
+        var graph = GraphParser.ParseNewGraph(fileName);
+        var algorithm = new CplexSolver(graph);
+        var result = algorithm.FindMaxClique();
+        Console.WriteLine(timer.Elapsed);
+        Console.WriteLine(result.Count);
+        Console.WriteLine(string.Join(",", result));
+        /*using (var writer = new StreamWriter("D:/VS Projects/CPLEX/stats.csv") { AutoFlush = true })
         {
-            var fileNames = new[] { "c-fat500-1.clq.txt", "c-fat500-2.clq.txt", "c-fat500-5.clq.txt", "c-fat200-5.clq.txt", "C125.9.clq.txt", "johnson16-2-4.clq.txt", "hamming8-4.clq.txt", "brock200_3.clq.txt", "brock200_4.clq.txt", "keller4.clq.txt" };
+            //var fileNames = new[] { "c-fat500-1.clq.txt", "c-fat500-2.clq.txt", "c-fat500-5.clq.txt", "c-fat200-5.clq.txt", "C125.9.clq.txt", "johnson16-2-4.clq.txt", "hamming8-4.clq.txt", "brock200_3.clq.txt", "brock200_4.clq.txt", "keller4.clq.txt" };
+            var fileNames = new[] { "keller4.clq.txt" };
             Parallel.ForEach(fileNames,
                 fileName =>
                 {
-                    writer.WriteLine(fileName);
                     var timer = Stopwatch.StartNew();
                     var graph = GraphParser.ParseNewGraph(fileName);
                     var algorithm = new CplexSolver(graph);
                     var result = algorithm.FindMaxClique();
                     lock (locker)
                     {
+                        Console.WriteLine(fileName);
+                        Console.WriteLine(timer.Elapsed);
+                        Console.WriteLine(result.Count);
+                        Console.WriteLine(string.Join(",", result));
                         writer.WriteLine(fileName);
                         writer.WriteLine(timer.Elapsed);
                         writer.WriteLine(result.Count);
                         writer.WriteLine(string.Join(",", result));
                     }
                 });
-        }
-        Console.WriteLine("Done");
+        }*/
         Console.ReadKey(false);
     }
 }
